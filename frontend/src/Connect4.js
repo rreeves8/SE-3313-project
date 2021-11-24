a7d50ce5fe6e49bb54d99376d8b6d12e7f2f51e8
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2021-11-24 17:16:37 -0500

    Server js win function

--- frontend/src/Connect4.js
+++ frontend/src/Connect4.js
@@ -1,6 +1,94 @@
 import './App.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "http://127.0.0.1:4001";
+
+function Cell(props){//react function for creating a circle element or cell, the color of the cell is passed through props
+    return <div className = 'cell'>
+               <div className = {props.color}>  
+               </div>
+           </div>
+}
+
+function Column(props){  //react function for creating a collum, each collum holds an array of cells. 
+    let newCells = []; //creating the holder of cells
+    for(let j = 0; j < 6; j++){ //filling the collum with cells or cirlcles, the props.cells contains the color of the corresponding cell in memory
+        newCells[j] =<Cell
+                        key = {j}
+                        y = {j}
+                        x = {props.x}
+                        color = {props.cells[j]} //assign the color of the cell from the collumns list of colors
+                      ></Cell>
+        
+    }
+    
+    return <div className="collumn" onClick = {() => props.handleClick()}> 
+               {newCells}
+           </div> // return the created collumn with the proper cell elements added, attatch the click handler to the collumn not the cells
+}
+
+
+class Grid extends Component {
+    constructor(){
+        super();
+        var newCells =  Array.from(Array(7), () => {
+            return new Array(6).fill('none')
+        })//create and empty double array with each color being none, or just blank
+        
+        this.state = { 
+            cells: newCells,
+            playerTurn: 'red',
+            winner: ''
+        } //set the initial state to the first player being red, no winner, and assign the empty cells array to the state
+    }
+
+    handleClick(columnNum) {        
+        if(this.state.winner === ''){  //if no winner lets check add another circle and check if theres a winner
+            for(let y = 6; y > -1; y--){ //cycle through the state double array, where the clicked collumn indicated which array to select
+                if(this.state.cells[columnNum][y] === 'none'){ //cycle upward through the collumn until and empty spot is found
+ 
+                    let prevState = this.state; //capture the current state and assign new values
+                    let newCells = this.state.cells;
+                    newCells[columnNum][y] = prevState.playerTurn; 
+
+                    this.setState({
+                        cells: newCells,    
+                        playerTurn: (prevState.playerTurn === 'red') ? 'black' : 'red',
+                        winner: this.checkWinner(newCells)
+                    })
+                    //set the new state and add the new color, when assigning the new winner state call the function check winner to see if theres a winer based on the new set of cells
+                    break;
+                }
+            }
+        }
+    }
+    
+    render(){    
+        let columns = [] //create an empty array to hold the collumn elements. assign the coloring of each cell or circle by passing the current state array in
+        for(let i = 0; i < 7; i++){
+            columns[i] =
+                <Column
+                    key = {i}
+                    x = {i}
+                    cells = {this.state.cells[i]}
+                    handleClick = {() => this.handleClick(i)}
+                ></Column>//send in click handler to the collumn and each array of cell colors to the corresponding collumn
+        }
+                
+        return(
+            <div>
+                <div className = 'grid'>
+                    {columns}
+                </div>
+                <div className = 'winner-text'> {this.state.winner} </div>
+            </div>
+        ); //create the header and the grid, the div element grid hols the collumns 
+  }
+}
+
+
 
 class Connect4 extends React.Component {
     render(){
@@ -8,9 +96,13 @@ class Connect4 extends React.Component {
             <div>
                 <a>Connect4</a>
                 <Link to = '/'> return home</Link>
+                <Grid/>
             </div>
         )
     }
 }
 
 export default Connect4;
+
+
+
--- server.js
+++ server.js
@@ -1,21 +1,218 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
+const io = require('socket.io')(3000)
 
+let socketStack = []
+
+let activeGames = []
+
+function newGame(playerId1, playerId2, color1, color2){
+    let cells =  Array.from(Array(7), () => {return new Array(6).fill('none')})
+
+    let player1 = {
+        playerId: playerId1,
+        color: color1
+    }
+
+    let player2 = {
+        playerId : playerId2,
+        color: color2 
+    }
+
+    let game = {
+        cells: cells,
+        turn: 'red',
+        winner: ''
+    }
+
+    activeGames.push({
+        p1: player1,
+        p2: player2,
+        game: game
+    })
+}
+
+function updateCells(playerId1, color, cooridnate){
+    let cells;
+    for(let i = 0; i < activeGames.length; i++){
+        if(activeGames[i].p1.playerId === playerId1){
+            activeGames[i].game.cells[cooridnate.x][cooridnate.y] = color;
+            cells = activeGames[i].game.cells;
+        }
+    }
+    let win = checkWinner(cells)
+
+    if(win !== ''){
+        return win;
+    }
+}
+
+function checkWinner(cells){
+    let cells;
+    for(let i = 0; i < activeGames.length; i++){
+        if(activeGames[i].p1.playerId === playerId1){
+            cells = activeGames[i].game.cells;
+        }
+    }
+
+    for(let x = 0; x < 7; x++){
+        for(let y = 0; y < 6; y++){
+            let win = checkCells(cells,x,y)
+            if(win !== ''){
+                return win;
+            }
+        }
+    }
+}
+
+function checkCells(cells, x, y){ 
+    let curr = cells[x][y];
+    let nextColor = '';
+    let count = 0; 
+
+    //vertical check
+    for(let i = 1; i < 4; i++){
+        nextColor = cells[x][y + i] 
+        if(nextColor === curr){
+            count++; 
+            if(count > 3){
+                return curr;
+            }
+        }
+        else{
+            break;
+        }
+    }
+
+    //horizontal check
+    for(let i = 1; i < 4; i++){
+        if(!(x + i > 6) && !(x-i<0)){ 
+            nextColor = cells[x + i][y];  
+            if(nextColor === data.curr){ 
+                count++; 
+                if(count > 3){
+                    return curr;
+                }
+            }
+            else{ 
+                break;
+            }
+        }    
+    }
+
+    //+x +y check
+    for(let i = 1; i < 4; i++){
+        if(!(x + i > 6) && !(y + i > 6) && !(x - i < 0) && !(y -i < 0)){//bounds check
+            nextColor = cells[x + i][y - i]
+            
+            if(nextColor === curr){
+                count++;
+                if(count > 3){
+                    return curr;
+                }
+            }
+            
+            else{
+                break;
+            }
+        }
+    }
+
+    for(let i = 1; i < 4; i++){
+        if(!(x + i > 6) && !(y + i > 6) && !(x - i < 0) && !(y -i < 0)){
+            nextColor = cells[x + i][data.y + i]
+            if(nextColor === curr){
+                count++;
+                if(count > 3){
+                    return curr;
+                }
+            }
+            else{
+                break;
+            }
+        }
+
+    }
+    
+    return ''
+}
+
+
+io.onconnection("connection", (socket) =>{
+
+})
+
+
+
+
+
+
+//steal code from below
+
+/*
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname+'/public/home/build/index.html'));
-});
-
-
-
-
+app.get('/api/getvalues', (request,response) => {
+    let conn = newConnection();
+    //get values in table, 
+})
+app.post('/api/setAvailable', jsonParser, (request,response) => {
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+});
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+app.get('/api/availability', (request,response) => {
+    let conn = newConnection();
+    conn.connect();
+    conn.query("SELECT * FROM availability;",(err,rows,feilds) => {
+        if(err){
+            response.send("error: " + err)
+            console.log(err)
+        }
+        else{
+            response.json(rows)
+        }
+    })
+    conn.end();
+})
+
+app.post('/api/setAvailable', jsonParser, (request,response) => {
+    let conn = newConnection();
+    conn.connect();
+    let data = request.body;
+    console.log(data)
+    let string = "INSERT INTO availability VALUES (";
+    
+    for(let i = 0; i < 10; i ++){
+        string += data[i];
+        if(i != 9){
+            string +=",";
+        } 
+    }
+
+    conn.query("DELETE FROM availability", (error,rows,fields) => {
+        console.log(error);
+    })
+
+    conn.query(string + ");", (error,rows,fields) => {
+        console.log(error);
+    })
+    conn.end();
+})
+
+
+
+*/
